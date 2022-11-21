4e0059143b03f46260cc3cd977576015c9b73225
Melissa Mischell <mmischel@unc.edu>
2022-11-21 16:01:12 -0500

    WIP start using regression to see if there is a difference in the slopes for obesity rates over time by demographics

--- demographic_exploration.R
+++ demographic_exploration.R
@@ -20,8 +20,26 @@ plot_demographic_over_time <- function(strat_id, title=""){
 
 # Gender over time
 nat_gender_plt <- plot_demographic_over_time('GEN', "Gender")
-
 ggsave("figures/national_gender_plt.png", nat_gender_plt)
+
+
+gen_data <- df %>%
+  filter(
+    (locationabbr == 'US') & 
+      (stratificationcategoryid1 == 'GEN') & 
+      (question == 'Percent of adults aged 18 years and older who have obesity')
+  ) %>%
+  arrange(yearstart)
+## twice-difference the gender obesity data
+fem_d2 <- diff(gen_data[gen_data$stratification1 == 'Female',]$data_value, differences = 1)
+male_d2 <- diff(gen_data[gen_data$stratification1 == 'Male',]$data_value, differences = 1)
+plot(fem_d2, ylab = expression("Female Obesity Rate"))
+plot(male_d2, ylab = expression("Male Obesity Rate"))
+
+
+# Linear Regression to compare slopes
+lm_model <- lm(data_value~yearstart + as.factor(stratification1), gen_data)
+summary(lm_model)
 
 # Income
 nat_2020_inc <- df %>%
@@ -53,9 +71,34 @@ inc_time_plt <- plot_demographic_over_time('INC', "Income")
 ggsave("figures/income_time_plt.png", inc_time_plt)
 
 
+inc_data <- df %>%
+  filter(
+    (locationabbr == 'US') & 
+      (stratificationcategoryid1 == 'INC') & 
+      (question == 'Percent of adults aged 18 years and older who have obesity')
+  ) %>%
+  arrange(yearstart)
+
+# Linear Regression to compare slopes
+lm_model_inc <- lm(data_value~yearstart + as.factor(stratification1) + yearstart*as.factor(stratification1), inc_data)
+summary(lm_model_inc)
+
 # Race/Ethnicity
 race_time_plt <- plot_demographic_over_time('RACE', "Race/Ethnicity")
 ggsave("figures/race_time_plt.png", race_time_plt)
+
+
+race_data <- df %>%
+  filter(
+    (locationabbr == 'US') & 
+      (stratificationcategoryid1 == 'RACE') & 
+      (question == 'Percent of adults aged 18 years and older who have obesity')
+  ) %>%
+  arrange(yearstart)
+
+# Linear Regression to compare slopes
+lm_model_race <- lm(data_value~yearstart + as.factor(stratification1) + yearstart*as.factor(stratification1), race_data)
+summary(lm_model_race)
 
 # Age
 age_time_plt <- plot_demographic_over_time('AGEYR', "Age")
